2b36d06cdd1f7733f6041e10051372e3adbddbe0
Rokas <rookas.rudzenskas@gmail.com>
2023-09-15 15:04:35 +0300

    🔧 chore(index.tsx): refactor code to improve readability and maintainability ✨ feat(index.tsx): add pose tracking functionality using react-native-reanimated and react-native-camera 🔒 chore(index.tsx): add camera permission request on component mount to ensure camera access

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -42,6 +42,57 @@ const defaultPose = {
 };
 
 const TabOneScreen = () => {
+  const pose = useSharedValue(defaultPose);
+
+  const leftWristToElbowPosition = usePosition(pose, 'leftWrist', 'leftElbow');
+  const leftElbowToShoulderPosition = usePosition(pose, 'leftElbow', 'leftShoulder');
+  const leftShoulderToHipPosition = usePosition(pose, 'leftShoulder', 'leftHip');
+  const leftHipToKneePosition = usePosition(pose, 'leftHip', 'leftKnee');
+  const leftKneeToAnklePosition = usePosition(pose, 'leftKnee', 'leftAnkle');
+
+  const rightWristToElbowPosition = usePosition(pose, 'rightWrist', 'rightElbow');
+  const rightElbowToShoulderPosition = usePosition(pose, 'rightElbow', 'rightShoulder');
+  const rightShoulderToHipPosition = usePosition(pose, 'rightShoulder', 'rightHip');
+  const rightHipToKneePosition = usePosition(pose, 'rightHip', 'rightKnee');
+  const rightKneeToAnklePosition = usePosition(pose, 'rightKnee', 'rightAnkle');
+
+  const shoulderToShoulderPosition = usePosition(pose, 'leftShoulder', 'rightShoulder');
+  const hipToHipPosition = usePosition(pose, 'leftHip', 'rightHip');
+
+  const dimensions = useWindowDimensions();
+
+  const frameProcessor = useFrameProcessor(frame => {
+    'worklet';
+    const poseObject = objectDetect(frame);
+
+    const xFactor = dimensions.width / frame.width;
+    const yFactor = dimensions.height / frame.height;
+
+    const poseCopy = {
+      leftShoulder: {x: 0, y: 0},
+      rightShoulder: {x: 0, y: 0},
+      leftElbow: {x: 0, y: 0},
+      rightElbow: {x: 0, y: 0},
+      leftWrist: {x: 0, y: 0},
+      rightWrist: {x: 0, y: 0},
+      leftHip: {x: 0, y: 0},
+      rightHip: {x: 0, y: 0},
+      leftKnee: {x: 0, y: 0},
+      rightKnee: {x: 0, y: 0},
+      leftAnkle: {x: 0, y: 0},
+      rightAnkle: {x: 0, y: 0},
+    };
+
+    Object.keys(poseObject).forEach(v => {
+      poseCopy[v] = {
+        x: poseObject[v].x * xFactor,
+        y: poseObject[v].y * yFactor,
+      };
+    });
+
+    pose.value = poseCopy;
+  }, []);
+
   const [cameraPermissionStatus, setCameraPermissionStatus] = useState<CameraPermissionStatus>('not-determined');
   const [microphonePermissionStatus, setMicrophonePermissionStatus] = useState<CameraPermissionStatus>('not-determined');
 
@@ -69,6 +120,13 @@ const TabOneScreen = () => {
 
   const devices = useCameraDevices()
   const device = devices.front
+
+  useEffect(() => {
+    const checkPermissions = async () => {
+      await Camera.requestCameraPermission();
+    };
+    checkPermissions();
+  }, []);
 
   if (device == null) return <></>
 
