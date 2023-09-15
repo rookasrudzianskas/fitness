7a56655d98343123ed055701ca47ef2502291cbe
Rokas <rookas.rudzenskas@gmail.com>
2023-09-15 15:02:37 +0300

    initializing the motion detection

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,7 +1,45 @@
 //@ts-nocheck
+import 'react-native-reanimated';
 import React, {useCallback, useEffect, useState} from 'react';
-import {Text, View, StyleSheet} from 'react-native';
-import {Camera, CameraPermissionStatus, useCameraDevices} from 'react-native-vision-camera';
+import {Dimensions, Platform, Text, View, StyleSheet, useWindowDimensions} from 'react-native';
+import {Camera, CameraPermissionStatus, useCameraDevices, useFrameProcessor} from 'react-native-vision-camera';
+import {useSharedValue, useAnimatedStyle} from 'react-native-reanimated';
+import Animated from 'react-native-reanimated';
+import Svg, {Line} from 'react-native-svg';
+
+const AnimatedLine = Animated.createAnimatedComponent(Line);
+
+const usePosition = (pose, valueName1, valueName2) => {
+  return useAnimatedStyle(
+    () => ({
+      x1: pose.value[valueName1].x,
+      y1: pose.value[valueName1].y,
+      x2: pose.value[valueName2].x,
+      y2: pose.value[valueName2].y,
+    }),
+    [pose],
+  );
+};
+
+export function objectDetect(frame) {
+  'worklet';
+  return __poseDetection(frame);
+}
+
+const defaultPose = {
+  leftShoulder: {x: 0, y: 0},
+  rightShoulder: {x: 0, y: 0},
+  leftElbow: {x: 0, y: 0},
+  rightElbow: {x: 0, y: 0},
+  leftWrist: {x: 0, y: 0},
+  rightWrist: {x: 0, y: 0},
+  leftHip: {x: 0, y: 0},
+  rightHip: {x: 0, y: 0},
+  leftKnee: {x: 0, y: 0},
+  rightKnee: {x: 0, y: 0},
+  leftAnkle: {x: 0, y: 0},
+  rightAnkle: {x: 0, y: 0},
+};
 
 const TabOneScreen = () => {
   const [cameraPermissionStatus, setCameraPermissionStatus] = useState<CameraPermissionStatus>('not-determined');
@@ -38,7 +76,7 @@ const TabOneScreen = () => {
     <View style={{
       flex: 1,
     }}>
-      <Text style={{color: 'white', text: 40}} onPress={requestCameraPermission}>
+      <Text style={{color: 'white'}} onPress={requestCameraPermission}>
         Video Grant
       </Text>
 
